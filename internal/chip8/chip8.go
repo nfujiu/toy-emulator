152fc32332e4cc273ac2d7bf23c97f55cb6dad45
fujiw <fujiw10@gmail.com>
2021-09-30 17:50:18 +0900

    [FEATURE] Load Ram

--- internal/chip8/chip8.go
+++ internal/chip8/chip8.go
@@ -5,6 +5,7 @@ import (
         "github.com/faiface/pixel/imdraw"
         "github.com/faiface/pixel/pixelgl"
         "golang.org/x/image/colornames"
+        "io/ioutil"
 )
 
 const (
@@ -18,17 +19,7 @@ type Chip8 struct {
         display Display
 }
 
-func Run () {
-        cfg := pixelgl.WindowConfig{
-                Title:  "chip8 Emulator",
-                Bounds: pixel.R(0, 0, windowWidth, windowHeight),
-                VSync: true,
-        }
-        win, err := pixelgl.NewWindow(cfg)
-        if err != nil {
-                panic(err)
-        }
-
+func handle (chip8 Chip8, win *pixelgl.Window) {
         for !win.Closed() {
                 win.Clear(colornames.Black)
 
@@ -48,4 +39,23 @@ func Run () {
                 }
                 win.Update()
         }
+}
+
+func Run () {
+        cfg := pixelgl.WindowConfig{
+                Title:  "chip8 Emulator",
+                Bounds: pixel.R(0, 0, windowWidth, windowHeight),
+                VSync: true,
+        }
+        win, err := pixelgl.NewWindow(cfg)
+        if err != nil {
+                panic(err)
+        }
+
+        file ,err := ioutil.ReadFile("../../roms/PONG")
+
+        chip8 := Chip8{}
+        chip8.ram.Load(file)
+
+        handle(chip8, win)
 }

7aa6ad431c8c60e2865b1dc5112649016244ec39
fujiw <fujiw10@gmail.com>
2021-09-30 20:13:38 +0900

    cpu instruction sets

--- internal/chip8/chip8.go
+++ internal/chip8/chip8.go
@@ -19,8 +19,78 @@ type Chip8 struct {
         display Display
 }
 
-func handle (chip8 Chip8, win *pixelgl.Window) {
+func (chip8 Chip8) dump() bool {
+        return true
+}
+
+func (chip8 *Chip8) tick() {
+        var pc uint16 =  uint16(0x200) + chip8.cpu.pc
+
+        var o1 uint8 = chip8.ram.buf[pc] >> 4
+        var o2 uint8 = chip8.ram.buf[pc] >> 0xf
+        var o3 uint8 = chip8.ram.buf[pc+1] >> 4
+        var o4 uint8 = chip8.ram.buf[pc+1] >> 0xf
+        println(o1)
+        println(o2)
+        println(o3)
+        println(o4)
+
+        switch o1 {
+        case 0x0:
+                println(o1, o2, o3, o4)
+        case 0x1:
+                println(o1, o2, o3, o4)
+        case 0x2:
+                println(o1, o2, o3, o4)
+        case 0x3:
+                println(o1, o2, o3, o4)
+        case 0x4:
+                println(o1, o2, o3, o4)
+        case 0x5:
+                println(o1, o2, o3, o4)
+        case 0x6:
+                println(o1, o2, o3, o4)
+        case 0x7:
+                println(o1, o2, o3, o4)
+        case 0x8:
+                println(o1, o2, o3, o4)
+        case 0x9:
+                println(o1, o2, o3, o4)
+        case 0xA:
+                println(o1, o2, o3, o4)
+        case 0xB:
+                println(o1, o2, o3, o4)
+        case 0xC:
+                println(o1, o2, o3, o4)
+        case 0xD:
+                println(o1, o2, o3, o4)
+        case 0xE:
+                println(o1, o2, o3, o4)
+        case 0xF:
+                println(o1, o2, o3, o4)
+        default:
+                println("Unknown opcode")
+        }
+        // opcode := uint16(chip8.ram.buf[pc]) << 8 | uint16(chip8.ram.buf[pc + 1])
+        // decode := opcode & 0xF000
+
+        // switch opcode & 0xF000 {
+        // case 0x6000:
+        //    println(decode)
+        // default:
+        //    println("Unknown opcode: 0x%X\n", opcode)
+        // }
+
+        // Chip-8 Instruction Set Architecture
+        // http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#3.1
+
+        chip8.cpu.Increment()
+}
+
+func handle(chip8 Chip8, win *pixelgl.Window) {
         for !win.Closed() {
+                chip8.tick()
+
                 win.Clear(colornames.Black)
 
                 for x := 0; x < 64; x++ {
@@ -41,7 +111,7 @@ func handle (chip8 Chip8, win *pixelgl.Window) {
         }
 }
 
-func Run () {
+func Run() {
         cfg := pixelgl.WindowConfig{
                 Title:  "chip8 Emulator",
                 Bounds: pixel.R(0, 0, windowWidth, windowHeight),
--- internal/chip8/cpu.go
+++ internal/chip8/cpu.go
@@ -4,5 +4,13 @@ type Cpu struct {
 		v [16]uint8
 		i uint16
 		sp uint8
-		pc uint8
+		pc uint16
+}
+
+func (c *Cpu) Increment() {
+		c.pc += 2
+}
+
+func (c *Cpu) Tick(ram Ram) {
+		println(c)
 }
